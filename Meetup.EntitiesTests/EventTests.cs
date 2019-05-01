d30d2e6083de044e38792f8aca5c1a01edfd2a7d
mort358d <mort358d@CV-BB-5860>
2019-05-01 10:08:17 +0200

    Made event tests

---
+++ Meetup.EntitiesTests/EventTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Meetup.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup.Entities.Tests
+{
+    [TestClass()]
+    public class EventTests
+    {
+        [TestMethod()]
+        public void EventTest()
+        {
+            //Test event name
+            new Event() { Name = "My 1st Event" };
+            Assert.ThrowsException<ArgumentException>(() => { new Event() { Name = "[MY EVENT] ~~Cool Event~~" }; }, "Invalid event name");
+
+            //Test event description
+            new Event() { Description = "My 783945uisdfsf{[]{£@$@$ event description" };
+            Assert.ThrowsException<ArgumentException>(() => { new Event() { Name = "     " }; }, "Invalid event description");
+        }
+
+        [TestMethod()]
+        public void GetUsersTest()
+        {
+            Event testEvent = new Event()
+            {
+                EventsUsers = new List<EventsUser>()
+                {
+                    new EventsUser() { User = new User() { FirstName = "Name" } },
+                    new EventsUser() { User = new User() { FirstName = "Person" } }
+                }
+            };
+
+            List<User> eventUsers = testEvent.GetUsers().ToList();
+            Assert.AreEqual(2, eventUsers.Count, "The list doesnt contain all the users in the event");
+            Assert.AreEqual("Person", eventUsers[1].FirstName, "The users arent correct");
+        }
+    }
+}
